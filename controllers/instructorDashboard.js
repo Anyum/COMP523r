0f08166dcd886e65e1f468b056e7f3fefc4abf76
Dessieman <brajer@live.unc.edu>
2017-03-09 01:24:58 -0500

    Update instructor dashboard. This is good example on how to pass JS objects to Pug.

--- controllers/instructorDashboard.js
+++ controllers/instructorDashboard.js
@@ -4,6 +4,8 @@
  */
 exports.index = (req, res) => {
     res.render('instructorDashboard', {
-        title: 'Instructor Dashboard'
+        title: 'Instructor Dashboard',
+        // TODO: implement ClientReqest.js MongoDB model, and write mongoose query here to count pending client requests
+        pendingClientRequests: '0'
     });
 };
