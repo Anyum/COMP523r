59d4b8b73fd873d1a73264b5888efdc50c71f21c
Dessieman <brajer@live.unc.edu>
2017-03-10 14:47:13 -0500

    Added TODO

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -7,7 +7,6 @@ const Client = require('../models/Client');
 exports.getDashboard = (req, res) => {
     // Count the number of client requests where instructor has not yet decided whether they are approved or not
     Client.count({isDecided: false}, (err, count) => {
-        console.log('Count is ' + count);
         res.render('instructorDashboard', {
             title: 'Instructor Dashboard',
             pendingClientRequests: count
