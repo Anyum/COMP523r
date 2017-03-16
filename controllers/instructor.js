d85ac4219c168903e74ada7c710f2ac31ef44290
ChrisBrajer <brajer@live.unc.edu>
2017-03-16 18:10:11 -0400

    Finished logic for instructor to POST approvals and denials for client submissions

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -7,6 +7,7 @@ const Client = require('../models/Client');
 exports.getDashboard = (req, res) => {
     // Count the number of client requests where instructor has not yet decided whether they are approved or not
     Client.count({isDecided: false}, (err, count) => {
+        if (err) return handleError(err);
         res.render('instructorDashboard', {
             title: 'Instructor Dashboard',
             pendingClientRequests: count
@@ -21,6 +22,7 @@ exports.getDashboard = (req, res) => {
 exports.getClientProposals = (req, res) => {
     Client.find({isDecided: false}, (err, Clients) => {
         // console.log(Clients);
+        if (err) return handleError(err);
         res.render('instructorClientProposals', {
             title: 'Review Client Proposals',
             clients: Clients
@@ -31,10 +33,30 @@ exports.getClientProposals = (req, res) => {
 /**
  * POST /instructor
  * Submit instructor's approval/denials.
+ * Change the client's values of isDecided and isApproved accordingly
  */
 exports.postClientProposals= (req, res) => {
-    // TODO: Implement this properly
-    console.log('Recieved request for ' + req.body.clientID);
-    console.log('This instructor ' + req.body.Decision + ' this request');
-    res.redirect('/instructor/client-proposals');
+    let decision = req.body.Decision;
+    let clientID = req.body.clientID;
+    console.log('Recieved request for ' + clientID);
+    console.log('This instructor ' + decision + ' this request');
+    // Find the client that the instructor approved/denied. Process CRUD.
+    Client.findOne({_id: clientID}, (err, client) => {
+        if (err) return handleError(err);
+        if (decision == 'Approve') {
+            client.isDecided = true;
+            client.isApproved = true;
+            client.save(function (err, client) {
+                if (err) { return res.status(500).send(err); }
+                else return res.redirect('/instructor/client-proposals');
+            });
+        } else {
+            client.isDecided = true;
+            client.isApproved = false;
+            client.save(function (err, client) {
+                if (err) { return res.status(500).send(err); }
+                else return res.redirect('/instructor/client-proposals');
+            });
+        }
+    });
 };
