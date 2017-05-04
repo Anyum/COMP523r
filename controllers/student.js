cd59960f6c03cbb568e8660f363d150caa186ac1
Abu-Bakar <aburaja@live.unc.edu>
2017-05-04 02:09:52 -0400

    Updated to normal student form functionality

--- controllers/student.js
+++ controllers/student.js
@@ -59,9 +59,6 @@ exports.postStudentForm = (req, res, next) => {
         return res.redirect('/student');
     }
 
-    Student.remove({},function(err, removed){
-    });
-
     const student = new Student({
         numStudents: req.body.numStudents,
         student1: req.body.student1,
