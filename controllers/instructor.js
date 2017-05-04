f695293060f71f21e1ea8634879a10f075fc2408
Abu-Bakar <aburaja@live.unc.edu>
2017-05-04 14:50:41 -0400

    final update (hopefully)

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -687,27 +687,9 @@ exports.getGeneratedTeams = (req, res) => {
 
 
         function removeTeams(callback) {
-            //GeneratedTeams.remove({});
             GeneratedTeams.remove({},function(err, removed){
             });
 
-        }
-        function addTeams(callback) {
-            for (var i = 0; i < t4.length; i++) {
-                t4[i].teamNumber = i;
-                const generatedTeam = new GeneratedTeams({
-                    teamNumber: i,
-                    assignedProject: "unassigned",
-                    numStudents: t4[i].numStudents,
-                    student1: t4[i].student1,
-                    student2: t4[i].student2,
-                    student3: t4[i].student3,
-                    student4: t4[i].student4,
-                    preferenceList: t4[i].preferenceList
-                });
-                generatedTeam.save();
-            }
-            callback();
         }
 
 
@@ -726,45 +708,17 @@ exports.getGeneratedTeams = (req, res) => {
                 preferenceList: t4[index].preferenceList
             });
             generatedTeam.save();
-            //callback();
         }
 
         operations.push(removeTeams);
         for (var i = 0; i < t4.length; i++) {
             operations.push(addTeam(i));
         }
-//operations.push(addTeams);
-
-
-        //operations.push(removeTeams);
-        //operations.push(addTeams);
 
         async.series(operations, function (err, results) {
-            // results[1]
-            // results[2]
+
         });
 
-
-        /*GeneratedTeams.remove({},function(err, removed){
-            if(err){
-                console.log(err);
-            }else{
-                for(var i=0; i<t4.length; i++){
-                    t4[i].teamNumber = i;
-                    const generatedTeam = new GeneratedTeams({
-                        teamNumber: i,
-                        assignedProject: "unassigned",
-                        numStudents: t4[i].numStudents,
-                        student1: t4[i].student1,
-                        student2: t4[i].student2,
-                        student3: t4[i].student3,
-                        student4: t4[i].student4,
-                        preferenceList: t4[i].preferenceList
-                    });
-                    generatedTeam.save();
-                }
-            }
-        });*/
         if(t4.length==12){
             canGenerateMapping = true;
         }
